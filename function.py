df13e720a8e6ed6fbf069d27a2d23fa39bcb54a9
Ren-Markus <ren-markus.kuznetsov@voco.ee>
2024-11-20 14:58:11 +0200

    For-tsükkel

---
+++ function.py
@@ -0,0 +1,22 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+
+# my_name_is(name)
+def my_name_is(name):
+    print(f"My name is {name}")
+
+# sum_six(num)`
+def sum_six(num):
+    return num + 6
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+
+# usd_to_eur()
+def usd_to_eur(usd):
+    return usd * 0.8
