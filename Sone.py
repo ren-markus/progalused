39c6c3fab40341a483940371abec334845f47b66
Ren-Markus <ren-markus.kuznetsov@voco.ee>
2024-11-20 20:21:21 +0200

    Ennik.py

---
+++ Sone.py
@@ -0,0 +1,11 @@
+# Write your code here
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + " " + last_name
+self_description_sentence = f"My name is {last_name}, {first_name}, {last_name}."
+cake = "vahukoormarjadtäidispõhi"
+print(f"{cake[0:8]}\n{cake[8:14]}\n{cake[14:20]}\n{cake[20:24]}")
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[10::-1]
